ba24bc92f10ec372944ae9a75f0418e042afb6b5
Bonjourempire <bonjourempire866@gmail.com>
2026-08-28 20:45:50 +0100

    Create evenements.js

---
+++ data/evenements.js
@@ -0,0 +1,15 @@
+const evenementsBonjourEmpire = [
+
+    {
+        titre: "Freestyle Universitaire",
+        date: "2026-09-29",
+        lieu: "À l'université de Kinshasa"
+    },
+
+    {
+        titre: "Afrik'arts — Édition 2",
+        date: "2026-10-17",
+        lieu: "UPN — Université pédagogique nationale"
+    }
+
+];
